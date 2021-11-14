7210a88cf4dfe7e5a866c0d51fb7910184e139f9
Humberto <humberto.galdino@live.com>
2021-11-14 02:01:26 -0300

    Instala Express

---
+++ app.js
@@ -0,0 +1,6 @@
+//Express
+const express = require('express');
+
+//Váriavel app, criada para execução da principal função do Express, ativando todas as suas funcionalidades.
+//Todos os métodos do Express estarão contidos dentro da variável
+const app = express();
